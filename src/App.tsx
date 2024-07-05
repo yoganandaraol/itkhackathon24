fb37bd54e82ffd82826291d614daf7ecac5696f8
Yogananda Rao Locherla <locherla@live.com>
2024-07-05 10:31:41 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-// src/App.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import Chat from './components/Chat';
 import PDFPreviewer from './components/PdfPreviewer';
+import { Box } from '@mui/material';
 
 const App: React.FC = () => {
   const fileUrl = 'Contract of Sale - Good 1.pdf';
+
   return (
-    <div className="App">
-      <header className="App-header">
-         {/* align center */}
-        <h1 style={{ textAlign: 'center' }}>Contract GPT</h1>
-      </header>
-      <main>
-        <Chat />
-        <div>
-            <h1>PDF Previewer</h1>
-            <PDFPreviewer fileUrl={fileUrl} />
+  <>
+<div className="App">
+            <header className="App-header">
+                <h1 style={{ textAlign: 'center' }}>COSA</h1>
+            </header>
+            <main className="main-container">
+                <div className="drag-drop">
+                    <p>Div with drag drop file feature</p>
+                </div>
+                <div className="content-container">
+                <Box sx={{ display: 'flex', flexDirection:'row', width: '100%' }}>
+                  <Box sx={{width:'50%'}}>
+                      <PDFPreviewer fileUrl={fileUrl} />
+                    </Box>
+                    <Box sx={{width:'50%'}}>
+                      <Chat />
+                    </Box>
+                    </Box>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
+      </>
   );
 };
 
